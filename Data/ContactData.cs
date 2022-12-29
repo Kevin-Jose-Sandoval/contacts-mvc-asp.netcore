29571a8c488ec40c918e2e29b0078dd4aca4ad7b
Kevin Sandoval <kevin.sandoval0026@gmail.com>
2022-12-28 22:30:41 -0600

    Add get, create and delete contact methods

--- Data/ContactData.cs
+++ Data/ContactData.cs
@@ -34,5 +34,117 @@ namespace ContactsApi.Data
 
             return list;
         }
+
+        public ContactModel GetContact(int contactId)
+        {
+            var newContact = new ContactModel();
+            var newConnection = new Connection();
+
+            using (var connection = new SqlConnection(newConnection.GetConnectionString()))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("sp_contact_getcontact", connection);
+                cmd.Parameters.AddWithValue("contactId", contactId);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        newContact.ContactId = Convert.ToInt32(dr["contactId"]);
+                        newContact.Name = dr["name"].ToString();
+                        newContact.Email = dr["email"].ToString();
+                        newContact.Phone = dr["phone"].ToString();
+                    }
+                }
+            }
+
+            return newContact;
+        }
+
+        public bool AddContact(ContactModel newContact)
+        {
+            bool result;
+
+            try
+            {
+                var newConnection = new Connection();
+
+                using (var connection = new SqlConnection(newConnection.GetConnectionString()))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("sp_contact_addcontact", connection);
+                    cmd.Parameters.AddWithValue("name", newContact.Name);
+                    cmd.Parameters.AddWithValue("phone", newContact.Phone);
+                    cmd.Parameters.AddWithValue("email", newContact.Email);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                result = true;
+            } catch (Exception e)
+            {
+                string error = e.Message;
+                result= false;
+            }
+
+            return result;
+        }
+
+        public bool UpdateContact(ContactModel newContact)
+        {
+            bool result;
+
+            try
+            {
+                var newConnection = new Connection();
+
+                using (var connection = new SqlConnection(newConnection.GetConnectionString()))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("sp_contact_updatecontact", connection);
+                    cmd.Parameters.AddWithValue("contactId", newContact.ContactId);
+                    cmd.Parameters.AddWithValue("name", newContact.Name);
+                    cmd.Parameters.AddWithValue("phone", newContact.Phone);
+                    cmd.Parameters.AddWithValue("email", newContact.Email);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                result = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool DeleteContact(int contactId)
+        {
+            bool result;
+
+            try
+            {
+                var newConnection = new Connection();
+
+                using (var connection = new SqlConnection(newConnection.GetConnectionString()))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("sp_contact_deletecontact", connection);
+                    cmd.Parameters.AddWithValue("contactId", contactId);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                result = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                result = false;
+            }
+
+            return result;
+        }
     }
 }
