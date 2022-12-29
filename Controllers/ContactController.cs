4ca819bf94d5e94d30ba9397cc9a3b3f153543b2
Kevin Sandoval <kevin.sandoval0026@gmail.com>
2022-12-29 16:44:35 -0600

    Create contact from view

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ContactsApi.Data;
+using ContactsApi.Models;
+
+namespace ContactsApi.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactData _ContactData = new ContactData();
+
+        public IActionResult List()
+        {
+            // return a contact list
+            var actualList = _ContactData.List();
+            return View(actualList);
+        }
+
+        public IActionResult Add()
+        {
+            // return only a view
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ContactModel newContact)
+        {
+            // receive an object save it to the database
+            if(!ModelState.IsValid)
+                return View();
+
+            var response = _ContactData.AddContact(newContact);
+            
+            if(response)
+                return RedirectToAction("List");
+            else
+                return View();
+        }
+    }
+}
--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -1,10 +1,15 @@
-﻿namespace ContactsApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsApi.Models
 {
     public class ContactModel
     {
         public int ContactId { get; set; }
+        [Required(ErrorMessage = "The name field is required")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "The phone field is required")]
         public string? Phone { get; set; }
+        [Required(ErrorMessage = "The email field is required")]
         public string? Email { get; set; }
     }
 }
